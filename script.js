f55f60ae02bb66958cbe9efad19b408021fa8b8b
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-03-08 01:41:51 -0500

    added variable values and restricted length input

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 // Assignment code here
+var char, num, low, cap, length;
+var alpha = 'abcdefghijklmnopqrstuvwxyz';
+var special = '!@#$%^&*?';
+var number = '0123456789';
+var temp = '';
+var password = '';
 
 var start = function () {
 length = window.prompt("How long would you like your password to be? Choose between 8-128.");
 
-  if (length < 8 || length > 128) {
-      window.alert("Length chosen is outside acceptable parameters. Please try again.");
+  if (isNaN(length) || length < 8 || length > 128) {
+    start();
   } else {
-      char = window.confirm("Would you like your password to contain special characters? (!@#$%^&*?)");
-      num = window.confirm("Would you like your password to contain numbers?");
-      low = window.confirm("Would you like your password to contain lower-case letters?");
-      cap = window.confirm("Would you like your password to contain upper-case letters?");
+    char = window.confirm("Would you like your password to contain special characters? (!@#$%^&*?)");
+    num = window.confirm("Would you like your password to contain numbers?");
+    low = window.confirm("Would you like your password to contain lower-case letters?");
+    cap = window.confirm("Would you like your password to contain upper-case letters?");
   }
 }
 start();
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
